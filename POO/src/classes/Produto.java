708e658c79fc84801aea49179bbc8f8e3e894ede
ferdinando Rainert <nandorainert2004@gmail.com>
2026-04-07 20:46:13 -0300

    Adicinando arquivos da aula de POO sobre construtur

---
+++ POO/src/classes/Produto.java
@@ -0,0 +1,19 @@
+package classes;
+
+public class Produto {
+	
+	public String nome;
+	public double preco;
+	public int quantidade;
+	
+	//Construtor
+	public Produto(String nome, double preco, int quantidade) {
+		
+		this.nome = nome;
+		this.preco = preco;
+		this.quantidade = quantidade;
+		
+		
+	}
+
+}
---
+++ POO/src/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+import classes.Produto;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Produto p1 = new Produto("Caderno", 12.00, 5);
+		Produto p2 = new Produto("Lápis", 5.00, 25);
+		
+		System.out.println(p1.nome);
+		System.out.println(p2.nome);
+	}
+
+}
